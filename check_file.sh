196237bef36ff77bf93c79b00f0c4512e483858a
Cian Phillips <cian@cca.edu>
2021-06-09 22:50:08 -0700

    updted to include header, etc

--- check_file.sh
+++ check_file.sh
@@ -1,4 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+	
+# Script to check and fix tdaccess files for workday import
+# Cian Phillips <cianphillips@gmail.com>
+# to lint: shellcheck -x check_file.sh  --shell=bash
+
+
+## Regular Expression used for checking the all space line
+## For some reason regex only allows 255 count of the previous string
+## Works well enough for our purposes
+padding_test="^[[:space:]]{255}"
+
+all_lines=""
+file_line=""
+bad_lines=""
+good_lines=""
+header_count=""
+footer_count=""
+
+set -euo pipefail
 
 ### Make sure the script is being called with an existing file as an argument
 if [[ -e $1 ]]; then
@@ -9,36 +28,17 @@ else
 	exit
 fi
 
-### Check to see if files downloaded from the DOE with TDClient are formatted to work
-### with Workday.
-### Sed is broken on Mac - should at least warn folks
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        echo "linux - we love linux!"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        echo "Looks like you are using a Mac."
-        echo "The default version of sed for Mac is broken."
-        echo "You can install GNU Sed via Homebrew to resolve the issue."
-        echo '(1) ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
-        echo "(2) brew update"
-        echo "(3) brew install gsed"
-        echo "(4) then you'll have to either rename sed to gsed below or make an alias pointing sed to gsed in your path."
-        echo " - or run it on a linux server or virtual machine you have access to."
-else
-        echo "Can't identify the Operating System type."
-fi
-
-
 ### Check to see if the lines in the file are 4300 characters
-file_line=""
 while IFS= read -r -u9 line; do
 	((file_line+=1))
 	((all_lines+=1))
-	if [ ${#line} != 4300 ]; then
-		echo $file_line:${#line}
+	if [[ ${#line} != 4300 ]]; then
+		echo "$file_line":${#line}
 		((bad_lines+=1))
 	else
 		((good_lines+=1))
 	fi
+
 ### Check to see if the file has headers and/or footers that break the Workday import
 	if [[ "${line}" =~ ^O\*N05 ]]; then
 		echo "Line $file_line is a header"
@@ -47,13 +47,26 @@ while IFS= read -r -u9 line; do
 		echo "Line $file_line is a footer"
 		((footer_count+=1))
 	fi
-done 9< $input_file
+
+## Check to see if file has the padding row of spaces after the header
+	if [[ ${line} =~ ${padding_test} ]]; then
+		echo "Line $file_line starts with 255 spaces."
+		if [[ $file_line = 1 ]]; then
+			echo "First line of all spaces looks good."
+		elif [[ $file_line = 2 ]]; then
+			echo "Second line looks like the padding, should be fine after stripping the header."
+		else
+			echo "Odd, found a space padded line found where not expected, check file."
+			exit
+		fi
+	fi
+done 9< "$input_file"
 
 ### If there are more than one header or one footer warn and quit
-if [[ $header_count > 1 ]]; then
+if [[ $header_count -gt 1 ]]; then
 	echo "$input_file has $header_count headers and will need to be split."
 	exit
-elif [[ ${footer_count} > 1 ]]; then
+elif [[ ${footer_count} -gt 1 ]]; then
 	echo "$input_file has $footer_count footers and will need to be split."
 	exit
 fi
@@ -66,17 +79,14 @@ elif [[ $bad_lines ]]; then
 fi
 
 ### If there are bad lines, offer to pad them with spaces
-if [[ $bad_lines > 0 ]]; then
+if [[ $bad_lines -gt 0 ]]; then
 	while true
 	do
 	 read -r -p "Would you like to pad all lines with spaces? [Y/n] " input
 	 case $input in
 	     [yY][eE][sS]|[yY])
 	echo "Adding spaces, this may take awhile..."
-### If you are using the Mac built-in version of sed this will fail
-### If you've installed gsed via Homebrew you can change sed to gsed
-### in the following line and it should work.
-	 sed -i ':a;/.\{4300\}/!{s/$/ /;ba}' ${input_file}
+	awk '{printf "%-4300s\n", $0}' "${input_file}" > tmp && mv tmp "${input_file}"
 	echo "Done adding spaces!"
 	 break
 	 ;;
@@ -99,8 +109,8 @@ if [[ $header_count = 1 ]] || [[ $footer_count = 1 ]]; then
 	 case $input in
 	     [yY][eE][sS]|[yY])
 	echo "Removing headers and footers..."
-	sed -i '/^O\*N05/ d' ${input_file}
-	sed -i '/^O\*N95/ d' ${input_file}
+	sed -i '/^O\*N05/ d' "${input_file}"
+	sed -i '/^O\*N95/ d' "${input_file}"
 	echo "Done stripping headers and footers!"
 	 break
 	 ;;
@@ -113,9 +123,10 @@ if [[ $header_count = 1 ]] || [[ $footer_count = 1 ]]; then
 	 ;;
 	 esac
 	done
+
 ### if there are more than one header or footer the file needs to be split
-elif [[ $header_count > 1 ]] || [[ $footer_count > 1 ]]; then
-	echo "Found more than one record in the file it should be split before processing."
+elif [[ $header_count -gt 1 ]] || [[ $footer_count -gt 1 ]]; then
+	echo "Found more than one record in the file, it should be split before processing."
 	exit
 else
 	echo "No headers or footers found!"
